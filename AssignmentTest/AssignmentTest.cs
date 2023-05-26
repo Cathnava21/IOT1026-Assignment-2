f6b482cea3ae4d9b7cfad8810812b7d2fc37e600
Cathnava21 <131034603+Cathnava21@users.noreply.github.com>
2023-05-26 23:03:20 +0000

    test2

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -6,6 +6,15 @@ namespace AssignmentTest
     public class AssignmentTests
     {
         [TestMethod]
+        public void LockedOpenTest()
+        {
+            // Create a new chest that is in the open state
+            TreasureChest chest = new TreasureChest(TreasureChest.State.Open);
+            chest.Lock();
+            Assert.AreEqual(chest.GetState(), TreasureChest.State.Open);
+        }
+
+        [TestMethod]
         public void OpenLockedTest()
         {
             // Chest starts in the locked state
@@ -36,7 +45,7 @@ namespace AssignmentTest
         {
             // Create a new chest that is in the Locked state
             TreasureChest chest = new TreasureChest(TreasureChest.State.Locked);
-            chest.Locked();
+            chest.Lock();
             Assert.AreEqual(chest.GetState(), TreasureChest.State.Locked);
         }
         [TestMethod]
@@ -44,15 +53,7 @@ namespace AssignmentTest
         {
             // Create a new chest that is in the Locked state
             TreasureChest chest = new TreasureChest(TreasureChest.State.Locked);
-            chest.Closed();
-            Assert.AreEqual(chest.GetState(), TreasureChest.State.Closed);
-        }
-        [TestMethod]
-        public void LockectOpenTest()
-        {
-            // Create a new chest that is in the open state
-            TreasureChest chest = new TreasureChest(TreasureChest.State.Open);
-            chest.Locked();
+            chest.Close();
             Assert.AreEqual(chest.GetState(), TreasureChest.State.Locked);
         }
     }
